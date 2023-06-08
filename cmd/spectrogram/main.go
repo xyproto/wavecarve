d44c22560c553af86998041f960c79175ee6ea1e
Alexander F. Rødseth <rodseth@gmail.com>
2023-06-08 15:49:04 +0200

    Update

---
+++ cmd/spectrogram/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"image/png"
+	"os"
+
+	"github.com/xyproto/wavecarve"
+)
+
+func main() {
+	fmt.Print("Reading example.wav...")
+
+	audioInts, _, err := wavecarve.ReadWavFile("example.wav")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("ok")
+	fmt.Print("Creating spectrogram...")
+
+	// A larger FFT size will give better frequency resolution
+	const fftSize = 512
+
+	spectrogram, err := wavecarve.CreateSpectrogramFromAudio(audioInts)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("ok")
+	fmt.Print("Writing spectogram.png...")
+
+	// Create the output file
+	spectrogramImageFile, err := os.Create("spectrogram.png")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+	defer spectrogramImageFile.Close()
+
+	// Encode the image to the output file
+	err = png.Encode(spectrogramImageFile, spectrogram)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("ok")
+	fmt.Print("Carving the spectrogram...")
+
+	carvedImage, err := wavecarve.CarveSeams(spectrogram, 50.0) // Reduce the width of the spectrogram by 50%
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not carve seams: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("ok")
+	fmt.Print("Writing carved.png...")
+
+	// Create the output file
+	carvedImageFile, err := os.Create("carved.png")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+	defer carvedImageFile.Close()
+
+	// Encode the image to the output file
+	err = png.Encode(carvedImageFile, carvedImage)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("ok")
+}
